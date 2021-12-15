51fdc33449fb0579017a9ee3603024d9f194d487
Swayam Chube <chubeswayam1701@gmail.com>
2021-12-15 17:51:58 +0530

    Cleanup in `server.py`

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket 
 import threading 
-from concurrent.futures import ThreadPoolExecutor
 import helper 
 
 client_list = []
@@ -26,25 +25,12 @@ class ClientHandler:
 		message = message.encode()
 		self.conn.sendall(message)
 
-def handle_client(conn, username):
-	while True:
-		message = conn.recv(1024)
-		message = message.decode('utf-8')
-		print(f"{username}> " + message)
-
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((helper.HOST, helper.PORT))
 serverSocket.listen()
-
-conn_list = list()
-
-executor = ThreadPoolExecutor(5)
 
 while True:
 	conn, addr = serverSocket.accept()
 	username = conn.recv(1024).decode('utf-8')
 	print(username)
-	# I now have an object for the client 
-	# Make a thread to handle the client
-	# executor.submit(handle_client, conn, username)
 	client_list.append(ClientHandler(conn, username))
